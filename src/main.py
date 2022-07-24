94b526a4c3ba05ee0d0c8bfe739c28f3ce6b0b91
John Sheehan <jennasyseng@gmail.com>
2022-07-24 16:46:31 -0700

    Changed to use MDCardSwipe

--- src/main.py
+++ src/main.py
@@ -1,27 +1,34 @@
 from kivymd.app import MDApp
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import NumericProperty, StringProperty
+from kivymd.uix.card import MDCardSwipe
 
 
-class Window(GridLayout):
-    pass
+class Book(MDCardSwipe):
+    book_id = NumericProperty()
+    text = StringProperty()
+    secondary_text = StringProperty()
 
+    def handle_select(self):
+        if self.open_progress == 0.0:
+            print(f"Clicked {self.book_id}!")
 
-class Book(BoxLayout):
-    @staticmethod
-    def on_press_button():
-        print('You pressed the button!')
+    def handle_delete(self):
+        if self.open_progress > 0.0:
+            print(f"Delete {self.book_id}!")
 
 
 class MainApp(MDApp):
     def build(self):
-        window = Window(rows=3)
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "Teal"
+        self.theme_cls.accent_palette = "Pink"
+        pass
 
-        item_list = window.ids.item_list
-        for _ in range(20):
-            item_list.add_widget(Book())
-
-        return window
+    def on_start(self):
+        for i in range(10):
+            self.root.ids.books.add_widget(
+                Book(book_id= i, text=f"Book title {i}", secondary_text="author")
+            )
 
 
 if __name__ == '__main__':
